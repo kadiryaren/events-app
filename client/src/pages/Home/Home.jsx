31fb11da4d4c94578f8cf654f61ec12fbe49c7aa
Kadir <kadiryaren20@gmail.com>
2023-03-04 18:10:15 +0300

    FIX: hero title colors and alignment fixed

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { SearchBar } from "../../component/index";
 const Home = () => {
 	return (
 		<div className="w-11/12 max-w-[100vh]  mx-auto">
+			
 			<SearchBar />
 			<div className="w-full h-[300vh] border-4 border-indigo-800">asdasd</div>
 		</div>
